21696a8b3f13ba36e18802e3ac2ba65027e16fbb
permafuture <permafuture@protonmail.com>
2018-10-10 16:26:19 -0600

    fuzzy-final

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,13 +14,14 @@ const IndexPage = () => (
 <p>We think that books are more important than ever. In a busy, distracted world, a real book is an oasis. Books change the way we think. They stretch our attention spans, challenge our opinions, transport us to other worlds. People who read books are more curious, more rational, and more kind.</p>
 <p>We&#39;re building a place for books and the people who love them. A place for community, right here in our beloved Nob Hill. A place away from the distractions of home and work. A gateway to other lands, a secret portal to the world of the book people. </p>
 <p>We&#39;re on a journey. Come with us.</p>
+  <p><em>[Put your email here for news]</em></p>
   </article>
 <div>
   <h1>Organic Books</h1>
   <p>
   <em>In the historic Nob Hill Shopping Center</em></p><p>
   111 Carlisle Boulevard SE</p>
-  <p><em>[Put your email here for news]</em></p>
+
   </div>
   </Layout>
 )
