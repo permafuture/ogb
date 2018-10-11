8c2c08df045b0e871c8c339cd0c075f8a543e61f
permafuture <permafuture@protonmail.com>
2018-10-11 11:14:21 -0600

    removed oxfor commma

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => (
     <h1>The new used bookstore in Nob Hill</h1>
   </div>
   <article>
-<p>We&#39;re the Brewer family: Steve, Kelly, Max, Seth, and Jalila. We love books. We&#39;ve been writing, reading, designing and editing for over a hundred years.</p>
+<p>We&#39;re the Brewer family: Steve, Kelly, Max, Seth and Jalila. We love books. We&#39;ve been writing, reading, designing and editing for over a hundred years.</p>
 <p>We think that books are more important than ever. In a busy, distracted world, a real book is an oasis. Books change the way we think. They stretch our attention spans, challenge our opinions, transport us to other worlds. People who read books are more curious, more rational, and more kind.</p>
 <p>We&#39;re building a place for books and the people who love them. A place for community, right here in our beloved Nob Hill. A place away from the distractions of home and work. A gateway to other lands, a secret portal to the world of the book people. </p>
 <p>We&#39;re on a journey. Come with us.</p>
